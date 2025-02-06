8d8cebea657097960639bb1d4ff532ab9d8c6a7b
AMI-lesson <aceami113@gmail.com>
2025-02-06 14:15:40 +0900

    作品２　HUMA 改良2/6

--- sakuhin2/HUMA/js/common.js
+++ sakuhin2/HUMA/js/common.js
@@ -1,4 +1,5 @@
 document.querySelector("#navControl a").addEventListener("click", function (e) {
     e.preventDefault(); // デフォルト動作を防ぐ
     document.querySelector(".l-header__nav").classList.toggle("is-active");
+    document.querySelector(".l-header__inner").classList.toggle("is-active");
 });
